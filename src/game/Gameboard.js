ff43a878455f44719cd0312356c0946cd55f45e8
Nivedh E R <nivedher12@gmail.com>
2026-06-15 19:21:47 +0530

    feat(initCoordinates): implemented this fn for initializing coordinates

--- src/game/Gameboard.js
+++ src/game/Gameboard.js
@@ -1,7 +1,17 @@
 export const Gameboard = () => {
+  const coordinates = [];
+  function initCoordinates() {
+    for (let i = 1; i <= 7; i++) {
+      for (let j = 1; j <= 7; j++) {
+        coordinates.push([i, j]);
+      }
+    }
+  }
   function placeShips() {}
+  initCoordinates();
   return {
-    missedAttacks,
+    coordinates,
+    missedAttacks: 0,
     receiveAttack(coordinates) {},
     reportSunkStatus() {},
   };
