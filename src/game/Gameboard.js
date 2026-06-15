bcfdc53d60375064e2e43ffce7fc922e204d35c0
Nivedh E R <nivedher12@gmail.com>
2026-06-15 21:33:51 +0530

    feat(Gameboard): implemented levels inside main coordinates

--- src/game/Gameboard.js
+++ src/game/Gameboard.js
@@ -2,13 +2,30 @@ export const Gameboard = () => {
   const coordinates = [];
   function initCoordinates() {
     for (let i = 1; i <= 7; i++) {
+      const coordinateLevels = [];
       for (let j = 1; j <= 7; j++) {
-        coordinates.push([i, j]);
+        coordinateLevels.push([i, j]);
+      }
+      coordinates.push(coordinateLevels);
+    }
+  }
+  function placeShips() {
+    let randomCoordinateLevel = Math.floor(Math.random() * coordinates.length);
+    let randomCoordinate = Math.floor(Math.random() * 7);
+    let ship = "Battleship";
+    let shipLength = 5;
+    while (shipLength !== 0) {
+      if (randomCoordinate + shipLength <= 7) {
+        coordinates[randomCoordinateLevel][randomCoordinate] = ship;
+        randomCoordinate++;
+        shipLength--;
+      } else {
+        throw new Error("Out of bounds");
       }
     }
   }
-  function placeShips() {}
   initCoordinates();
+  placeShips();
   return {
     coordinates,
     missedAttacks: 0,
@@ -16,3 +33,6 @@ export const Gameboard = () => {
     reportSunkStatus() {},
   };
 };
+
+const game = Gameboard();
+console.log(game.coordinates);
