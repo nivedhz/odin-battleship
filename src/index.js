06259b7484411d37f514e0c7153e97df1a821a12
Nivedh E R <nivedher12@gmail.com>
2026-06-24 19:33:53 +0530

    chore(import): removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { initHandlers } from "./handlers/init-delegated-events.js";
 import "./styles/style.css";
 import { initStartScreen } from "./ui/start-screen.js";
-import { gameScreen } from "./ui/game-screen.js";
 
 const DOM = {
   container: document.querySelector(".container"),
