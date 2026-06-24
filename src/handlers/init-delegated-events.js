0364bf4e084c617f24749f6375bd59fd71fa555e
Nivedh E R <nivedher12@gmail.com>
2026-06-24 17:32:40 +0530

    feat(gameScreen): implemented the gameScreen with both boards

--- src/handlers/init-delegated-events.js
+++ src/handlers/init-delegated-events.js
@@ -1,6 +1,7 @@
 import { state } from "../data/state.js";
 import GameBoard from "../game/GameBoard.js";
 import Player from "../game/Player.js";
+import { gameScreen } from "../ui/game-screen.js";
 import { initStartScreen } from "../ui/start-screen.js";
 
 export function initHandlers(container) {
@@ -13,6 +14,9 @@ export function initHandlers(container) {
       document
         .querySelector(".start-screen__board-container")
         .replaceChildren(initStartScreen(container).createSelectionBoard());
+    }
+    if (e.target.matches(".start-screen__start-btn")) {
+      container.replaceChildren(gameScreen().createGameScreen());
     }
   });
 }
--- src/ui/game-screen.js
+++ src/ui/game-screen.js
@@ -1,52 +1,91 @@
 import { state } from "../data/state.js";
-import "../styles/start-screen.css";
+import "../styles/game-screen.css";
 import bombImg from "../assets/bomb.svg";
 
-export const gameScreen = (container) => {
+export const gameScreen = () => {
   function createPlayerBoard(player) {
     const boardContainer = document.createElement("div");
-    boardContainer.classList.add("start-screen__board-container");
+    boardContainer.classList.add("game-screen__player-board-container");
+    const heading = document.createElement("h1");
+    heading.classList.add("game-screen__player-heading");
+    heading.textContent = "Player";
+    const board = document.createElement("div");
+    board.classList.add("game-screen__player-board");
     let levelCoord = 0;
     let gridCoord = 0;
     player.gameboard.coordinates.forEach((level) => {
       level.forEach((coordinate) => {
         const gridElem = document.createElement("div");
         if (typeof coordinate === "number") {
-          state.player.gameboard.receiveAttack([5, 2]);
-          state.player.gameboard.receiveAttack([4, 2]);
-          state.player.gameboard.receiveAttack([3, 2]);
           if (coordinate === 0) {
-            gridElem.classList.add("start-screen__grid-elem");
+            gridElem.classList.add("game-screen__player-grid-elem");
           } else {
-            gridElem.classList.add("start-screen__water-elem");
+            gridElem.classList.add("game-screen__player-water-elem");
             gridElem.textContent = "\u2022";
           }
         } else {
           if (
-            state.player.gameboard.attackedSpot.has(
+            player.gameboard.attackedSpot.has(
               JSON.stringify([levelCoord, gridCoord]),
             )
           ) {
-            gridElem.classList.add("start-screen__sunk-ship-elem");
+            gridElem.classList.add("game-screen__player-sunk-ship-elem");
             const bombImgContainer = document.createElement("img");
             bombImgContainer.classList.add("sunk-ship-elem__bomb-img");
             bombImgContainer.src = bombImg;
             gridElem.append(bombImgContainer);
-          } else gridElem.classList.add("start-screen__ship-elem");
+          } else gridElem.classList.add("game-screen__player-ship-elem");
         }
-        boardContainer.append(gridElem);
+        board.append(gridElem);
         gridCoord++;
       });
       levelCoord++;
       gridCoord = 0;
     });
 
+    boardContainer.append(heading, board);
+    return boardContainer;
+  }
+  function createComputerBoard(player) {
+    const boardContainer = document.createElement("div");
+    boardContainer.classList.add("game-screen__computer-board-container");
+    const heading = document.createElement("h1");
+    heading.classList.add("game-screen__computer-heading");
+    heading.textContent = "Computer";
+    const board = document.createElement("div");
+    board.classList.add("game-screen__computer-board");
+    let levelCoord = 0;
+    let gridCoord = 0;
+    player.gameboard.coordinates.forEach((level) => {
+      level.forEach((coordinate) => {
+        const gridElem = document.createElement("div");
+        if (typeof coordinate === "number") {
+          if (coordinate === 0) {
+            gridElem.classList.add("game-screen__computer-grid-elem");
+          }
+        } else {
+          gridElem.classList.add("game-screen__computer-ship-elem");
+        }
+        board.append(gridElem);
+        gridCoord++;
+      });
+      levelCoord++;
+      gridCoord = 0;
+    });
+
+    boardContainer.append(heading, board);
     return boardContainer;
   }
   function createGameScreen() {
+    const gameScreenContainer = document.createElement("div");
+    gameScreenContainer.classList.add("game-screen__container");
     const userBoard = createPlayerBoard(state.player);
-    const computerBoard = createPlayerBoard(state.computer);
-    container.append(userBoard, computerBoard);
+    const computerBoard = createComputerBoard(state.computer);
+    gameScreenContainer.append(userBoard, computerBoard);
+
+    return gameScreenContainer;
   }
-  createGameScreen();
+  return {
+    createGameScreen,
+  };
 };
